fe874aa06c3d6e119f6ba0834b11e052f1af9f4f
sam <samliao1@auforce.com>
2024-03-12 11:40:47 +0800

    feat: 新增比對 GameList.xlsx 內的順序

--- src/util.ts
+++ src/util.ts
@@ -1,33 +1,92 @@
-import { WebDriver, WebElement, By } from "selenium-webdriver";
+import { WebDriver, WebElement, By } from 'selenium-webdriver';
 
+/**
+ * 輔助方法
+ */
 export class Utils {
-    static async findElementClassName(webDriver: WebDriver, className: string, ms: number, action: string): Promise<WebElement> {
-        try {
-            const element = await webDriver.findElement(By.className(className));
+  /**
+   * 找到瀏覽器下指定 class 名稱的元素
+   * @param webDriver
+   * @param className
+   * @param ms
+   * @param action
+   * @returns
+   */
+  static async findAllClassName(
+    webDriver: WebDriver,
+    className: string,
+    ms: number,
+    action: string,
+  ): Promise<WebElement> {
+    try {
+      const element = await webDriver.findElement(By.className(className));
 
-            switch (action) {
-                case 'click':
-                    await element.click();
-                    break;
-            }
-            // 等待一段時間，確保所有動態生成內容載入完成
-            await webDriver.sleep(ms); // ms 毫秒為延遲時間（ms/1000 秒）
-            return element;
-        }
-        catch (error) {
-            console.log(error);
-        }
+      switch (action) {
+        case 'click':
+          await element.click();
+          break;
+      }
+      // 等待一段時間，確保所有動態生成內容載入完成
+      await webDriver.sleep(ms); // ms 毫秒為延遲時間（ms/1000 秒）
+      return element;
+    } catch (error) {
+      console.log(error);
     }
-    static async findElementText(webDriver: WebDriver, webElement: WebElement, text: string, ms: number, action: string): Promise<WebElement> {
-        const element = await webElement.findElement(By.xpath(`//*[contains(text(), '${text}')]`));
+  }
+  /**
+   * 找到指定 text 內容的元素
+   * @param driver
+   * @param webElement
+   * @param text
+   * @param ms
+   * @param action
+   * @returns
+   */
+  static async findElementText(
+    driver: WebDriver,
+    webElement: WebElement,
+    text: string,
+    ms: number,
+    action: string,
+  ): Promise<WebElement> {
+    const element = await webElement.findElement(
+      By.xpath(`//*[contains(text(), '${text}')]`),
+    );
 
-        switch (action) {
-            case 'click':
-                await element.click();
-                break;
-        }
-        // 等待一段時間，確保所有動態生成內容載入完成
-        await webDriver.sleep(ms); // ms 毫秒為延遲時間（ms/1000 秒）
-        return element;
+    switch (action) {
+      case 'click':
+        await element.click();
+        break;
     }
+    // 等待一段時間，確保所有動態生成內容載入完成
+    await driver.sleep(ms); // ms 毫秒為延遲時間（ms/1000 秒）
+    return element;
+  }
+  /**
+   * 找到瀏覽器下指定 text 內容的元素
+   * @param driver
+   * @param text
+   * @param ms
+   * @param action
+   * @returns
+   */
+  static async findAllText(
+    driver: WebDriver,
+    text: string,
+    ms: number,
+    action: string,
+  ): Promise<WebElement> {
+    const element = await driver.findElement(
+      By.xpath(`//*[contains(text(), '${text}')]`),
+    );
+
+    switch (action) {
+      case 'click':
+        await element.click();
+        break;
+    }
+    // 等待一段時間，確保所有動態生成內容載入完成
+    await driver.sleep(ms); // ms 毫秒為延遲時間（ms/1000 秒）
+    return element;
+  }
 }
--- src/web-page-compare.service.ts
+++ src/web-page-compare.service.ts
@@ -1,16 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { Builder, Capabilities } from 'selenium-webdriver';
 import { By } from 'selenium-webdriver';
-import { Utils } from "./util";
+import { Utils } from './util';
+import * as XLSX from 'xlsx';
 
 @Injectable()
 export class WebPageCompareService {
-  constructor() { }
+  constructor() {}
 
   /**
    * 為了測試執行結果，可加入 `onApplicationBootstrap()` 方法來執行程式。
    */
   async readWebPage(options?: { gameType: 'slot' }) {
+    // 讀取 Excel 檔案
+    const workbook = XLSX.readFile('GameList.xlsx');
+
+    // 獲取第一個工作表
+    const sheet_name_list = workbook.SheetNames;
+    const worksheet = workbook.Sheets[sheet_name_list[0]];
+
+    // 將工作表轉換為 JSON 對象
+    const data = XLSX.utils.sheet_to_json(worksheet);
+
+    const map = new Map();
+    data.forEach((element, index) => {
+      const gameName = (element as { gameName: string }).gameName;
+      if (map.has(gameName)) {
+        throw new Error(`Map already contain the key(${gameName}).`);
+      } else {
+        map.set(gameName, index);
+      }
+    });
+
     // 創建一個 WebDriver 實例，這裡使用 Chrome 瀏覽器
     const driver = await new Builder()
       .withCapabilities(Capabilities.chrome())
@@ -31,35 +52,50 @@ export class WebPageCompareService {
       // 等待一段時間，確保所有動態生成內容載入完成
       await driver.sleep(2000); // 2000 毫秒為延遲時間（2秒）
 
-      const are_lang = await Utils.findElementClassName(
-        driver, 'are_lang', 200, 'click'
+      const are_lang = await Utils.findAllClassName(
+        driver,
+        'are_lang',
+        200,
+        'click',
       );
 
       // 點選【繁體中文】選項切換為繁體中文
-      const lnk_child = await Utils.findElementText(
-        driver, are_lang, '繁體中文', 200, 'click'
-      );
+      await Utils.findElementText(driver, are_lang, '繁體中文', 200, 'click');
 
       // 點選【電子】
-      const li_nav = await driver.findElement(
-        By.xpath("//*[contains(text(), '電子')]"),
+      await Utils.findAllText(driver, '電子', 200, 'click');
+
+      const panel_wrap = await Utils.findAllClassName(
+        driver,
+        'panel_wrap',
+        1,
+        '',
       );
-      li_nav.click();
 
-      // 等待一段時間，確保所有動態生成內容載入完成
-      await driver.sleep(2000); // 2000 毫秒為延遲時間（2秒）
-
-      const panel_wrap = await driver.findElement(By.className('panel_wrap'));
       const panel_name = await panel_wrap.findElements(
         By.className('panel_name'),
       );
 
       let count = 1;
-      panel_name.forEach(async (element) => {
+      for (const element of panel_name) {
         const text = await element.getText();
-        console.log(count + ' text: ' + text);
+
+        if (map.has(text)) {
+          const index = map.get(text);
+          const nowIndex = index + 1;
+          if (nowIndex != count) {
+            console.log(
+              `text: ${text} 排序錯誤 Excel=${nowIndex}, 遊戲畫面=${count}`,
+            );
+          } else {
+            console.log(`${count} text: ${text} 正確✅`);
+          }
+        } else {
+          console.log(`text: ${text} 不存在`);
+        }
+
         count += 1;
-      });
+      }
 
       // 取得網頁標題並輸出
       const title = await driver.getTitle();
@@ -69,14 +105,11 @@ export class WebPageCompareService {
       //const pageSource = await driver.getPageSource();
       //console.log('網頁內容:', pageSource);
     } finally {
-      // 等待一段時間，讓人預覽後再關閉瀏覽器
-      await driver.sleep(10000); // 10000 毫秒為延遲時間（10秒）
-
       // 關閉瀏覽器
       await driver.quit();
 
-      // 停止伺服器
-      console.log('Server stopped');
+      // 關閉伺服器
+      console.log('關閉伺服器');
       process.exit(0); // 關閉應用程式
     }
   }
