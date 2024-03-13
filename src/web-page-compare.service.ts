8094e48afbf309c4976a8e6cba359b97f15e2a4b
santino <santinochang@auforce.com>
2024-03-13 11:41:55 +0800

    feat: 添加頁籤點選功能

--- src/web-page-compare.service.ts
+++ src/web-page-compare.service.ts
@@ -23,24 +23,6 @@ export class WebPageCompareService {
     console.log(`CLUB_TYPE=${clubType}`);
 
     // 讀取 Excel 檔案
-    const workbook = XLSX.readFile('GameList.xlsx');
-
-    // 獲取第一個工作表
-    const sheet_name_list = workbook.SheetNames;
-    const worksheet = workbook.Sheets[sheet_name_list[0]];
-
-    // 將工作表轉換為 JSON 對象
-    const data = XLSX.utils.sheet_to_json(worksheet);
-
-    const map = new Map();
-    data.forEach((element, index) => {
-      const gameName = (element as { gameName: string }).gameName;
-      if (map.has(gameName)) {
-        throw new Error(`Map already contain the key(${gameName}).`);
-      } else {
-        map.set(gameName, index);
-      }
-    });
 
     // 創建一個 WebDriver 實例，這裡使用 Chrome 瀏覽器
     const driver = await new Builder()
@@ -92,32 +74,75 @@ export class WebPageCompareService {
 
       await Utils.findElementText(driver, aside_list, clubType, 200, 'click');
 
-      // 取的遊戲列表
-      const panel_name = await panel_wrap.findElements(
-        By.className('panel_name'),
-      );
+      const cycleCheck = async (sheetIndex: number) => {
+        const workbook = XLSX.readFile('GameList.xlsx');
 
-      let count = 1;
-      for (const element of panel_name) {
-        const text = await element.getText();
+        // 獲取第一個工作表
+        const sheet_name_list = workbook.SheetNames;
 
-        if (map.has(text)) {
-          const index = map.get(text);
-          const nowIndex = index + 1;
-          if (nowIndex != count) {
-            console.log(
-              `❌ text: ${text} 排序錯誤 Excel=${nowIndex}, 遊戲畫面=${count}`,
-            );
+        const worksheet = workbook.Sheets[sheet_name_list[sheetIndex]];
+        // 將工作表轉換為 JSON 對象
+        const data = XLSX.utils.sheet_to_json(worksheet);
+        const categoryType = sheet_name_list[sheetIndex];
+        console.log(`CATEGORY_TYPE=${categoryType}`);
+        // 點選【頁籤】
+        const category_list = await Utils.findAllClassName(
+          driver,
+          'tabs',
+          1,
+          '',
+        );
+
+        await Utils.findElementText(
+          driver,
+          category_list,
+          categoryType,
+          200,
+          'click',
+        );
+        // 取的遊戲列表
+        const panel_name = await panel_wrap.findElements(
+          By.className('panel_name'),
+        );
+
+        const map = new Map();
+
+        data.forEach((element, index) => {
+          const gameName = (element as { gameName: string }).gameName;
+          if (map.has(gameName)) {
+            throw new Error(`Map already contain the key(${gameName}).`);
           } else {
-            console.log(`${count} text: ${text} 正確✅`);
+            map.set(gameName, index);
           }
-        } else {
-          console.log(`❌ text: ${text} Excel中不存在`);
+        });
+        let count = 1;
+        for (const element of panel_name) {
+          const text = await element.getText();
+          if (map.has(text)) {
+            const index = map.get(text);
+            const nowIndex = index + 1;
+            if (nowIndex != count) {
+              console.log(
+                `❌ text: ${text} 排序錯誤 Excel=${nowIndex}, 遊戲畫面=${count}`,
+              );
+            } else {
+              console.log(`${count} text: ${text} 正確✅`);
+            }
+          } else {
+            console.log(`❌ text: ${text} Excel中不存在`);
+          }
+
+          count += 1;
         }
-
-        count += 1;
-      }
-
+      };
+      await cycleCheck(0);
+      await driver.sleep(500);
+      await cycleCheck(1);
+      await driver.sleep(500);
+      await cycleCheck(2);
+      await driver.sleep(500);
+      await cycleCheck(3);
+      await driver.sleep(500);
       // 取得網頁標題並輸出
       const title = await driver.getTitle();
       console.log('網頁標題:', title);
